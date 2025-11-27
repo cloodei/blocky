dd4665dc6d07acf290b321fe4e05a3894ae15983
Nguyễn Việt Quốc An <40015342+cloodei@users.noreply.github.com>
2025-11-27 22:55:25 +0700

    init

---
+++ l1/xorci.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+typedef __int128 i128;
+using namespace std;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    
+
+    return 0;
+}
