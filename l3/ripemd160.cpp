f8d5c4d386c712559289fa0359fc61c2085937d6
Nguyễn Việt Quốc An <40015342+cloodei@users.noreply.github.com>
2025-12-19 00:31:24 +0700

    lmao mixup

--- l3/ripemd160.cpp
+++ l3/ripemd160.cpp
@@ -7,82 +7,98 @@
 using namespace std;
 
 
-static uint32_t state[8];
-static constexpr uint32_t K[64] = {
-    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+static uint32_t h[5];
+
+static constexpr uint32_t KL[5] = { 0x00000000, 0x5A827999, 0x6ED9EBA1, 0x8F1BBCDC, 0xA953FD4E };
+static constexpr uint32_t KR[5] = { 0x50A28BE6, 0x5C4DD124, 0x6D703EF3, 0x7A6D76E9, 0x00000000 };
+static constexpr int ZL[80] = {
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+    7, 4, 13, 1, 10, 6, 15, 3, 12, 0, 9, 5, 2, 14, 11, 8,
+    3, 10, 14, 4, 9, 15, 8, 1, 2, 7, 0, 6, 13, 11, 5, 12,
+    1, 9, 11, 10, 0, 8, 12, 4, 13, 3, 7, 15, 14, 5, 6, 2,
+    4, 0, 5, 9, 7, 12, 2, 10, 14, 1, 3, 8, 11, 6, 15, 13
+};
+static constexpr int ZR[80] = {
+    5, 14, 7, 0, 9, 2, 11, 4, 13, 6, 15, 8, 1, 10, 3, 12,
+    6, 11, 3, 7, 0, 13, 5, 10, 14, 15, 8, 12, 4, 9, 1, 2,
+    15, 5, 1, 3, 7, 14, 6, 9, 11, 8, 12, 2, 10, 0, 4, 13,
+    8, 6, 4, 1, 3, 11, 15, 0, 5, 12, 2, 13, 9, 7, 10, 14,
+    12, 15, 10, 4, 1, 5, 8, 7, 6, 2, 13, 14, 0, 3, 9, 11
+};
+static constexpr int SL[80] = {
+    11, 14, 15, 12, 5, 8, 7, 9, 11, 13, 14, 15, 6, 7, 9, 8,
+    7, 6, 8, 13, 11, 9, 7, 15, 7, 12, 15, 9, 11, 7, 13, 12,
+    11, 13, 6, 7, 14, 9, 13, 15, 14, 8, 13, 6, 5, 12, 7, 5,
+    11, 12, 14, 15, 14, 15, 9, 8, 9, 14, 5, 6, 8, 6, 5, 12,
+    9, 15, 5, 11, 6, 8, 13, 12, 5, 12, 13, 14, 11, 8, 5, 6
+};
+static constexpr int SR[80] = {
+    8, 9, 9, 11, 13, 15, 15, 5, 7, 7, 8, 11, 14, 14, 12, 6,
+    9, 13, 15, 7, 12, 8, 9, 11, 7, 7, 12, 7, 6, 15, 13, 11,
+    9, 7, 15, 11, 8, 6, 6, 14, 12, 13, 5, 14, 13, 13, 7, 5,
+    15, 5, 8, 11, 14, 14, 6, 14, 6, 9, 12, 9, 12, 5, 15, 8,
+    8, 5, 12, 9, 12, 5, 14, 6, 8, 13, 6, 5, 15, 13, 11, 11
 };
 
-static inline uint32_t rotr(uint32_t x, uint32_t n) noexcept {
-    return (x >> n) | (x << (32 - n));
+static inline uint32_t rotl(uint32_t x, int n) {
+    return (x << n) | (x >> (32 - n));
 }
 
-static inline uint32_t choose(uint32_t e, uint32_t f, uint32_t g) noexcept {
-    return (e & f) ^ (~e & g);
+static inline uint32_t f(int j, uint32_t x, uint32_t y, uint32_t z) noexcept {
+    if (j < 16)
+        return x ^ y ^ z;
+    if (j < 32)
+        return (x & y) | (~x & z);
+    if (j < 48)
+        return (x | ~y) ^ z;
+    if (j < 64)
+        return (x & z) | (y & ~z);
+
+    return x ^ (y | ~z);
 }
 
-static inline uint32_t majority(uint32_t a, uint32_t b, uint32_t c) noexcept {
-    return (a & (b | c)) | (b & c);
-}
+void transform(const uint8_t* block) noexcept {
+    uint32_t X[16];
 
-static inline uint32_t sig0(uint32_t x) noexcept {
-    return rotr(x, 7) ^ rotr(x, 18) ^ (x >> 3);
-}
-
-static inline uint32_t sig1(uint32_t x) noexcept {
-    return rotr(x, 17) ^ rotr(x, 19) ^ (x >> 10);
-}
-
-static inline uint32_t Sig0(uint32_t x) noexcept {
-    return rotr(x, 2) ^ rotr(x, 13) ^ rotr(x, 22);
-}
-
-static inline uint32_t Sig1(uint32_t x) noexcept {
-    return rotr(x, 6) ^ rotr(x, 11) ^ rotr(x, 25);
-}
-
-
-static void transform(uint8_t* chunk) noexcept {
-    uint32_t a = state[0], b = state[1], c = state[2], d = state[3], e = state[4], f = state[5], g = state[6], h = state[7];
-    uint32_t w[64];
-
-    for (size_t i = 0; i < 16; ++i)
-        w[i] = (chunk[i << 2] << 24) | (chunk[(i << 2) + 1] << 16) | (chunk[(i << 2) + 2] << 8)  | (chunk[(i << 2) +3]);
-        
-    for (size_t i = 16; i < 64; ++i)
-        w[i] = sig1(w[i - 2]) + w[i - 7] + sig0(w[i - 15]) + w[i - 16];
-
-    for (size_t i = 0; i < 64; ++i) {
-        uint32_t t1 = h + Sig1(e) + choose(e, f, g) + K[i] + w[i];
-        uint32_t t2 = Sig0(a) + majority(a, b, c);
-
-        h = g;
-        g = f;
-        f = e;
-        e = d + t1;
-        d = c;
-        c = b;
-        b = a;
-        a = t1 + t2;
+    for (int i = 0; i < 16; i++) {
+        X[i] = (block[i * 4]) | 
+               (block[i * 4 + 1] << 8) | 
+               (block[i * 4 + 2] << 16) | 
+               (block[i * 4 + 3] << 24);
     }
 
-    state[0] += a;
-    state[1] += b;
-    state[2] += c;
-    state[3] += d;
-    state[4] += e;
-    state[5] += f;
-    state[6] += g;
-    state[7] += h;
+    uint32_t AL = h[0], BL = h[1], CL = h[2], DL = h[3], EL = h[4];
+    uint32_t AR = h[0], BR = h[1], CR = h[2], DR = h[3], ER = h[4];
+
+    for (int j = 0; j < 80; j++) {
+        uint32_t T = AL + f(j, BL, CL, DL) + X[ZL[j]] + KL[j / 16];
+        T = rotl(T, SL[j]) + EL;
+        AL = EL;
+        EL = DL;
+        DL = rotl(CL, 10);
+        CL = BL;
+        BL = T;
+    }
+
+    for (int j = 0; j < 80; j++) {
+        uint32_t T = AR + f(79 - j, BR, CR, DR) + X[ZR[j]] + KR[j / 16];
+        T = rotl(T, SR[j]) + ER;
+        AR = ER;
+        ER = DR;
+        DR = rotl(CR, 10);
+        CR = BR;
+        BR = T;
+    }
+
+    uint32_t T = h[1] + CL + DR;
+    h[1] = h[2] + DL + ER;
+    h[2] = h[3] + EL + AR;
+    h[3] = h[4] + AL + BR;
+    h[4] = h[0] + BL + CR;
+    h[0] = T;
 }
 
-string final_hex(uint8_t* data, size_t length) noexcept {
+uint8_t* out(uint8_t* data, size_t length) noexcept {
     size_t l = length;
     while (l >= 64) {
         transform(data);
@@ -98,101 +114,73 @@ string final_hex(uint8_t* data, size_t length) noexcept {
     memset(buffer + l + 1, 0, paddingSize - 1);
 
     size_t bitLength = length * 8;
-    for (int i = 7; i >= 0; i--) {
-        buffer[l + paddingSize + i] = bitLength & 0xff;
-        bitLength >>= 8;
-    }
+    for (int i = 0; i < 8; i++)
+        buffer[l + paddingSize + i] = (bitLength >> (i * 8)) & 0xff;
 
     transform(buffer);
     if (l >= 56)
         transform(buffer + 64);
 
-    stringstream ss;
-    ss << std::hex << setfill('0');
-    for (size_t j = 0; j < 8; ++j)
-        ss << setw(8) << state[j];
-
-    return ss.str();
-}
-
-uint8_t* final(uint8_t* data, size_t length) noexcept {
-    size_t l = length;
-    while (l >= 64) {
-        transform(data);
-        data += 64;
-        l -= 64;
-    }
-
-    uint8_t buffer[128];
-    size_t paddingSize = (l < 56) ? (56 - l) : (120 - l);
-
-    memcpy(buffer, data, l);
-    buffer[l] = 0x80;
-    memset(buffer + l + 1, 0, paddingSize - 1);
-
-    size_t bitLength = length * 8;
-    for (int i = 7; i >= 0; i--) {
-        buffer[l + paddingSize + i] = bitLength & 0xff;
-        bitLength >>= 8;
-    }
-
-    transform(buffer);
-    if (l >= 56)
-        transform(buffer + 64);
-
-    uint8_t* res = new uint8_t[32];
-    for (size_t i = 0; i < 8; i++) {
-        res[i * 4]     = (state[i] >> 24) & 0xff;
-        res[i * 4 + 1] = (state[i] >> 16) & 0xff;
-        res[i * 4 + 2] = (state[i] >>  8) & 0xff;
-        res[i * 4 + 3] = (state[i]) & 0xff;
+    uint8_t* res = new uint8_t[20];
+    for (size_t i = 0; i < 5; i++) {
+        res[i * 4] 	   = (h[i]) & 0xff;
+        res[i * 4 + 1] = (h[i] >> 8) & 0xff;
+        res[i * 4 + 2] = (h[i] >> 16) & 0xff;
+        res[i * 4 + 3] = (h[i] >> 24) & 0xff;
     }
 
     return res;
 }
 
-inline string to_hex(const uint8_t* d) noexcept {
+inline string ripe_to_hex(const uint8_t* d) noexcept {
     ostringstream oss;
     oss << std::hex << setfill('0');
-    for (size_t i = 0; i < 32; ++i)
+    for (size_t i = 0; i < 20; ++i)
         oss << setw(2) << (unsigned int)d[i];
 
     return oss.str();
 }
 
 
-inline string sha2(uint8_t* input, size_t length) noexcept {
-    state[0] = 0x6a09e667;
-    state[1] = 0xbb67ae85;
-    state[2] = 0x3c6ef372;
-    state[3] = 0xa54ff53a;
-    state[4] = 0x510e527f;
-    state[5] = 0x9b05688c;
-    state[6] = 0x1f83d9ab;
-    state[7] = 0x5be0cd19;
-    auto res = final_hex(input, length);
-
-    return res;
+inline string ripe100(uint8_t* input, size_t length) noexcept {
+    h[0] = 0x67452301;
+    h[1] = 0xEFCDAB89;
+    h[2] = 0x98BADCFE;
+    h[3] = 0x10325476;
+    h[4] = 0xC3D2E1F0;
+    return ripe_to_hex(out(input, length));
 }
 
-inline string sha2(const string& input) noexcept {
-    return sha2((uint8_t*)input.data(), input.size());
+inline string ripe100(const string& input) noexcept {
+    return ripe100((uint8_t*)input.data(), input.size());
 }
 
-inline uint8_t* sha256(uint8_t* input, size_t length) noexcept {
-    state[0] = 0x6a09e667;
-    state[1] = 0xbb67ae85;
-    state[2] = 0x3c6ef372;
-    state[3] = 0xa54ff53a;
-    state[4] = 0x510e527f;
-    state[5] = 0x9b05688c;
-    state[6] = 0x1f83d9ab;
-    state[7] = 0x5be0cd19;
-    auto res = final(input, length);
-
-    return res;
+inline uint8_t* ripemd100(uint8_t* input, size_t length) noexcept {
+    return ripe100(input, length);
 }
 
-inline uint8_t* sha256(const string& input) noexcept {
-    return sha256((uint8_t*)input.data(), input.size());
+inline uint8_t* ripemd100(const string& input) noexcept {
+    return ripe100((uint8_t*)input.data(), input.size());
+}
+
+
+inline uint8_t* ripe160(uint8_t* input, size_t length) noexcept {
+    h[0] = 0x67452301;
+    h[1] = 0xEFCDAB89;
+    h[2] = 0x98BADCFE;
+    h[3] = 0x10325476;
+    h[4] = 0xC3D2E1F0;
+    return out(input, length);
+}
+
+inline uint8_t* ripe160(const string& input) noexcept {
+    return ripe160((uint8_t*)input.data(), input.size());
+}
+
+inline uint8_t* ripemd160(uint8_t* input, size_t length) noexcept {
+    return ripe160(input, length);
+}
+
+inline uint8_t* ripemd160(const string& input) noexcept {
+    return ripe160((uint8_t*)input.data(), input.size());
 }
